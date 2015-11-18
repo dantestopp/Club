0be843145e735910ed15b7b9d2584309076854ad
happyoniens <dante.stoppini@gmail.com>
2015-11-18 20:21:29 +0100

    Revert "Ignore Config File"
    This reverts commit d896ada8243909e802c8d1bbe440e030f3184878.

--- controllers/teamController.php
+++ controllers/teamController.php
@@ -2,72 +2,72 @@
 
 class teamController{
 
-    public function getTeamWithId($id){
-          $sql = "SELECT * FROM team WHERE id = '$id'";
-          $result = Flight::db()->query($sql);
-          return new team($result->fetch_assoc());
+public function getTeamWithId($id){
+      $sql = "SELECT * FROM team WHERE id = '$id'";
+      $result = Flight::db()->query($sql);
+      return new team($result->fetch_assoc());
+  }
+
+public function getAllTeams(){
+      $sql = "SELECT * FROM team";
+      $result = Flight::db()->query($sql);
+      $teams = array();
+      while($row = $result->fetch_assoc()){
+        $teams[] = new team($row);
       }
+      return $teams;
+  }
 
-    public function getAllTeams(){
-          $sql = "SELECT * FROM team";
-          $result = Flight::db()->query($sql);
-          $teams = array();
-          while($row = $result->fetch_assoc()){
-            $teams[] = new team($row);
-          }
-          return $teams;
-      }
+public function getTeamNameWithId($id){
+    $sql = "SELECT name FROM team WHERE id = '$id'";
+    $result = Flight::db()->query($sql);
+    return $result->fetch_assoc()['name'];
+  }
 
-    public function getTeamNameWithId($id){
-        $sql = "SELECT name FROM team WHERE id = '$id'";
-        $result = Flight::db()->query($sql);
-        return $result->fetch_assoc()['name'];
-      }
+public function showTeamList(){
+    Flight::auth()->check();
 
-    public function showTeamList(){
-        Flight::auth()->check();
+    $teams = Flight::auth()->currentUser->getTeams();
+    if(count($teams) == 1){
+      Flight::redirect('/team/'.$teams[0]->id);
+    }
 
-        $teams = Flight::auth()->currentUser->getTeams();
-        if(count($teams) == 1){
-          Flight::redirect('/team/'.$teams[0]->id);
-        }
+    Flight::util()->render('teams',array("team_count"=>count($teams),"teams"=>$teams));
+  }
 
-        Flight::util()->render('teams',array("team_count"=>count($teams),"teams"=>$teams));
-      }
+public function showTeam($id){
+    Flight::auth()->check();
 
-    public function showTeam($id){
-        Flight::auth()->check();
+    $team = Flight::teams()->getTeamWithId($id);
+    Flight::util()->render('team',array("team"=>$team,
+                                        "coaches"=>$team->getCoaches(),
+                                        "players"=>$team->getPlayers(),
+                                        "events"=>$team->getEvents()
+                                      ));
+  }
 
-        $team = Flight::teams()->getTeamWithId($id);
-        Flight::util()->render('team',array("team"=>$team,
-                                            "coaches"=>$team->getCoaches(),
-                                            "players"=>$team->getPlayers(),
-                                            "events"=>$team->getEvents()
-                                          ));
-      }
+public function saveNewTeam(){
+    Flight::auth()->check(20);
+    $response = Flight::util()->validate('team',Flight::request()->data);
+    if(is_array($response)){
+      Flight::util()->render('newTeam',array("error"=>$response));
+      return;
+    }
+    $team = new team(Flight::request()->data);
+    $last_id = $team->store();
+    Flight::redirect('/team/'.$last_id);
+  }
 
-    public function saveNewTeam(){
-        Flight::auth()->check(20);
-        $response = Flight::util()->validate('team',Flight::request()->data);
-        if(is_array($response)){
-          Flight::util()->render('newTeam',array("error"=>$response));
-          return;
-        }
-        $team = new team(Flight::request()->data);
-        $last_id = $team->store();
-        Flight::redirect('/team/'.$last_id);
-      }
+public function showNewTeam(){
+    Flight::auth()->check(20);
+    Flight::util()->render('newTeam');
+  }
 
-    public function showNewTeam(){
-        Flight::auth()->check(20);
-        Flight::util()->render('newTeam');
-      }
+public function deleteTeam($id){
+    Flight::auth()->check(20);
+    $team = Flight::teams()->getTeamWithId($id);
+    $team->delete();
 
-    public function deleteTeam($id){
-        Flight::auth()->check(20);
-        $team = Flight::teams()->getTeamWithId($id);
-        $team->delete();
-
-        Flight::redirect('/teams');
-      }
+    Flight::redirect('/teams');
+  }
 }
